47c7d054c1fcf4dba765e5af9eaae44ed91656f0
Tianrui Ma <catrinamtr@gmail.com>
2018-02-02 22:43:03 -0500

    sidebar demo

---
+++ CodeTrip/app/src/main/java/com/mtr/codetrip/myapplication/SideBarActivity.java
@@ -0,0 +1,17 @@
+package com.mtr.codetrip.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Catrina on 2/2/2018.
+ */
+
+public class SideBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sidebar);
+    }
+}
