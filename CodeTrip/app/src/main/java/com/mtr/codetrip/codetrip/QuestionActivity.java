accd2e04bd8333b7a852ede0ca27a3718e95d29d
Tianrui Ma <catrinamtr@gmail.com>
2018-02-23 20:19:10 -0500

    question activity return button

--- CodeTrip/app/src/main/java/com/mtr/codetrip/codetrip/QuestionActivity.java
+++ CodeTrip/app/src/main/java/com/mtr/codetrip/codetrip/QuestionActivity.java
@@ -146,7 +146,8 @@ public class QuestionActivity extends FragmentActivity implements View.OnClickLi
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.retrun_button){
-
+            // save state
+            this.finish();
         }else if(id == R.id.hint_button){
             // Initialize a new instance of LayoutInflater service
             LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
@@ -154,19 +155,6 @@ public class QuestionActivity extends FragmentActivity implements View.OnClickLi
             // Inflate the custom layout/view
             View customView = inflater.inflate(R.layout.hint,null);
 
-                /*
-                    public PopupWindow (View contentView, int width, int height)
-                        Create a new non focusable popup window which can display the contentView.
-                        The dimension of the window must be passed to this constructor.
-
-                        The popup does not provide any background. This should be handled by
-                        the content view.
-
-                    Parameters
-                        contentView : the popup's content
-                        width : the popup's width
-                        height : the popup's height
-                */
             // Initialize a new instance of popup window
             mPopupWindow = new PopupWindow(
                     customView,
@@ -175,7 +163,6 @@ public class QuestionActivity extends FragmentActivity implements View.OnClickLi
             );
 
             // Set an elevation value for popup window
-            // Call requires API level 21
             if(Build.VERSION.SDK_INT>=21){
                 mPopupWindow.setElevation(5.0f);
             }
@@ -192,23 +179,8 @@ public class QuestionActivity extends FragmentActivity implements View.OnClickLi
                 }
             });
 
-                /*
-                    public void showAtLocation (View parent, int gravity, int x, int y)
-                        Display the content view in a popup window at the specified location. If the
-                        popup window cannot fit on screen, it will be clipped.
-                        Learn WindowManager.LayoutParams for more information on how gravity and the x
-                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
-                        to specifying Gravity.LEFT | Gravity.TOP.
-
-                    Parameters
-                        parent : a parent view to get the getWindowToken() token from
-                        gravity : the gravity which controls the placement of the popup window
-                        x : the popup's x location offset
-                        y : the popup's y location offset
-                */
             // Finally, show the popup window at the center location of root relative layout
             mPopupWindow.showAtLocation(mLinearLayout, Gravity.CENTER,0,0);
-
         }
     }
 
