0fb8b507fbf68d1d007d772f0498a90e014467d8
Jiahua Zhu <j66zhu@uwaterloo.ca>
2018-02-23 19:31:56 -0500

    Added Question class

---
+++ CodeTrip/app/src/main/java/com/mtr/codetrip/codetrip/Question.java
@@ -0,0 +1,15 @@
+package com.mtr.codetrip.codetrip;
+
+import android.view.View;
+
+/**
+ * Created by j66zhu on 2018-02-23.
+ */
+
+public class Question {
+
+    enum Question_Type{Rearrange, MultipleChoice, Insert, DragNDrop};
+    private View view;
+    private Question_Type type;
+
+}
