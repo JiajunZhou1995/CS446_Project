0466f8755f3832dce7e38acb029d07a3bee67cbe
xiaoxiaoxiaoxiaowan <anthony.wuhaolin@gmail.com>
2018-02-25 22:16:40 -0500

    Knowledge Bar

---
+++ CodeTrip/app/src/main/java/com/mtr/codetrip/codetrip/ExpandAdapter.java
@@ -0,0 +1,132 @@
+package com.mtr.codetrip.codetrip;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+
+/**
+ * Created by Yolo on 2018-02-24.
+ */
+
+public class ExpandAdapter extends BaseExpandableListAdapter{
+    private static final String TAG = "NormalExpandableListAda";
+    private String[] groupData;
+    private String[][] childData;
+    private onExpandListener mOnGroupExpandedListener;
+    
+
+    public ExpandAdapter(String[] groupData, String[][] childData) {
+        this.groupData = groupData;
+        this.childData = childData;
+    }
+
+
+    public void setOnGroupExpandedListener(onExpandListener onGroupExpandedListener) {
+        mOnGroupExpandedListener = onGroupExpandedListener;
+    }
+
+
+
+
+
+    @Override
+    public int getGroupCount() {
+        return groupData.length;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return childData[groupPosition].length;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return groupData[groupPosition];
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return childData[groupPosition][childPosition];
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View
+            convertView, ViewGroup parent) {
+        GroupViewHolder groupViewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.expandlistview, parent, false);
+            groupViewHolder = new GroupViewHolder();
+            groupViewHolder.tvTitle = (TextView) convertView.findViewById(R.id.label_group_normal);
+            convertView.setTag(groupViewHolder);
+        } else {
+            groupViewHolder = (GroupViewHolder) convertView.getTag();
+        }
+        groupViewHolder.tvTitle.setText(groupData[groupPosition]);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View
+            convertView, ViewGroup parent) {
+        ChildViewHolder childViewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.childexpand, parent, false);
+            childViewHolder = new ChildViewHolder();
+            childViewHolder.tvTitle = (TextView) convertView.findViewById(R.id.label_expand_child);
+            convertView.setTag(childViewHolder);
+        } else {
+            childViewHolder = (ChildViewHolder) convertView.getTag();
+        }
+
+
+        childViewHolder.tvTitle.setText(childData[groupPosition][childPosition]);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        if (mOnGroupExpandedListener != null) {
+            mOnGroupExpandedListener.onExpandListener(groupPosition);
+        }
+    }
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+    }
+
+    private static class GroupViewHolder {
+        TextView tvTitle;
+    }
+
+    private static class ChildViewHolder {
+        TextView tvTitle;
+    }
+}
+
---
+++ CodeTrip/app/src/main/java/com/mtr/codetrip/codetrip/onExpandListener.java
@@ -0,0 +1,11 @@
+package com.mtr.codetrip.codetrip;
+
+/**
+ * Created by Yolo on 2018-02-24.
+ */
+
+public interface onExpandListener {
+
+    void onExpandListener(int groupPosition);
+
+}
